1cdc32357e0fc203c7a5cfc1464d7e1ab08f6c2a
Javohir Bozorov <87217569+JovaDev@users.noreply.github.com>
2021-08-17 18:23:08 +0500

    SpeakEnglish botining kodlari

---
+++ main.py
@@ -0,0 +1,52 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+
+from oxfordLookup import getDefinitions
+from googletrans import Translator
+translator = Translator()
+
+API_TOKEN = "1934275471:AAE5f2bXkDTNoTIfryoaDkEh101w5JIj-0E"
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands='start')
+async def send_welcome(message: types.Message):
+    await message.reply("Salom!\n<b>Speak English Botiga</b>ga xush kelibsiz!", parse_mode='HTML')
+
+
+@dp.message_handler(commands='help')
+async def send_welcome(message: types.Message):
+    await message.reply("Menga so'zlarni yuboring va men uni inglizchaga tarjima qilib <b>izoh</b> va <b>audio</b>sini yuboraman.", parse_mode='HTML')
+
+
+@dp.message_handler()
+async def tarjimon(message: types.Message):
+    print(message)
+    lang = translator.detect(message.text).lang
+    if len(message.text.split()) > 1:
+        dest = 'uz' if lang == 'en' else 'en'
+        await message.reply(translator.translate(message.text, dest).text)
+    else:
+        if lang=='en':
+            word_id = message.text
+        else:
+            word_id = translator.translate(message.text, dest='en').text
+
+        lookup = getDefinitions(word_id)
+        if lookup:
+            await message.reply(f"Word: {word_id} \nDefinitions:\n{lookup['definitions']}")
+            if lookup.get('audio'):
+                await message.reply_audio(lookup['audio'])
+        else:
+            await message.reply("Bunday so'z topilmadi")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
+#
